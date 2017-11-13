07c57914860ec31cdd9494cc0a9902da734d3b24
Drashti Panchal <drpanchal93@gmail.com>
2017-11-13 13:32:41 +0530

    BorderPaneDashboard changed: Scroll Pane added.

--- src/erp/BorderPaneDashboardController.java
+++ src/erp/BorderPaneDashboardController.java
@@ -6,12 +6,17 @@
 package erp;
 
 import com.jfoenix.controls.JFXButton;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -46,9 +51,28 @@ public class BorderPaneDashboardController implements Initializable {
     private JFXButton SalesButton;
     
     @FXML
-    void SalesButtonClick(ActionEvent event)
+    private JFXButton PurchaseButton;
+    
+    @FXML
+    private AnchorPane contentAnchorPane;
+
+    @FXML
+    private ScrollPane contentScrollPane;
+    
+    @FXML
+    void SalesButtonClick(ActionEvent event) throws IOException
     {
         //content.getChildren().setAll(FXMLLoader.load(/Users/drashtipanchal/Documents/Drashti/NetBeansProjects/src/erp));
+        Parent salesData = FXMLLoader.load(getClass().getResource("SalesDataForm.fxml"));
+        contentScrollPane.setContent(salesData);
+    }
+    
+    @FXML
+    void PurchaseButtonClick(ActionEvent event) throws IOException
+    {
+        //content.getChildren().setAll(FXMLLoader.load(/Users/drashtipanchal/Documents/Drashti/NetBeansProjects/src/erp));
+        Parent salesData = FXMLLoader.load(getClass().getResource("PurchaseDataForm.fxml"));
+        contentScrollPane.setContent(salesData);
     }
     
     
