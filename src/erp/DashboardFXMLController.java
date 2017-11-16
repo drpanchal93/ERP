961aa59e267f63e1a666c6d87c302c1045b3068d
drpanchal93 <drpanchal93@gmail.com>
2017-11-16 10:38:00 +0530

    ImageView made clickable

--- src/erp/DashboardFXMLController.java
+++ src/erp/DashboardFXMLController.java
@@ -8,15 +8,21 @@ package erp;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
@@ -36,13 +42,28 @@ public class DashboardFXMLController implements Initializable {
      
      @FXML
     private Circle userProfileImage;
+     
+      @FXML
+    private Button SalesButton;
+      
+       @FXML
+    private ImageView testImage;
     
      
     @FXML
     void SalesButtonClick(ActionEvent event) throws IOException {
-        Parent goToSceneTwo = FXMLLoader.load(getClass().getResource("SalesDashboard.fxml"));
+        OpenSalesDashboard(event);
+    }
+    
+    void OpenSalesDashboard(Event e) {
+        Parent goToSceneTwo = null;
+            try {
+                goToSceneTwo = FXMLLoader.load(getClass().getResource("SalesDashboard.fxml"));
+            } catch (IOException ex) {
+                Logger.getLogger(DashboardFXMLController.class.getName()).log(Level.SEVERE, null, ex);
+            }
             Scene SalesDashboard = new Scene(goToSceneTwo);
-            Stage app_Stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Stage app_Stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
             app_Stage.setScene(SalesDashboard);
             //Css code
             //SalesDashboard.getStylesheets().add(DashboardController.class.getResource("salesdashboard.css").toExternalForm());
@@ -54,6 +75,11 @@ public class DashboardFXMLController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        testImage.setPickOnBounds(true); // allows click on transparent areas
+        testImage.setOnMouseClicked((MouseEvent e) -> {
+            System.out.println("Clicked!"); // change functionality
+            OpenSalesDashboard(e);
+        });
     }    
     
 }
